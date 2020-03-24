d7a24335dda7957693b91865d459aa295474b85f
Dalton <faker20@up.edu>
2020-03-23 19:28:37 -0700

    Checkpoint 1.

---
+++ app/src/androidTest/java/up/edu/dimcalc/TwoPointsTest.java
@@ -0,0 +1,34 @@
+package up.edu.dimcalc;
+
+import static org.junit.Assert.*;
+
+public class TwoPointsTest {
+
+    @org.junit.Test
+    public void getPoint() {
+    }
+
+    @org.junit.Test
+    public void setPoint() {
+    }
+
+    @org.junit.Test
+    public void randomValue() {
+    }
+
+    @org.junit.Test
+    public void setOrigin() {
+    }
+
+    @org.junit.Test
+    public void copy() {
+    }
+
+    @org.junit.Test
+    public void distance() {
+    }
+
+    @org.junit.Test
+    public void slope() {
+    }
+}
